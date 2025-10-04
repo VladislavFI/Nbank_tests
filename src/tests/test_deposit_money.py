a3fade9b3c11aa497d70cd6a37cd241addb24541
Vladislav Firsenko <vlad.4834bc@mail.ru>
2025-10-04 14:57:18 +0700

    Добавил проверку существующих счетов

--- src/tests/test_deposit_money.py
+++ src/tests/test_deposit_money.py
@@ -1,12 +1,19 @@
 import pytest
 import requests
 
-BASE_URL= "http://localhost:4111/api/v1"
+BASE_URL = "http://localhost:4111/api/v1"
 AUTH_HEADER = {
     'accept': 'application/json',
     'Authorization': 'Basic a2F0ZTE5OTg3Nzp2ZXJ5c1RSb25nUGFzc3dvcmQzMyQ=',
     'Content-Type': 'application/json'
 }
+
+AUTH_HEADER_ADMIN = {
+    'accept': 'application/json',
+    'Authorization': 'Basic YWRtaW46YWRtaW4=',
+    'Content-Type': 'application/json'
+}
+
 
 @pytest.mark.api
 class TestDepositMoney:
@@ -19,8 +26,7 @@ class TestDepositMoney:
         )
 
         old_balance = next(acc["balance"] for acc in customer_profile.json()["accounts"]
-                                if acc["id"] == 2)
-
+                           if acc["id"] == 2)
 
         # Делаем депозит
         response = requests.post(
@@ -41,7 +47,6 @@ class TestDepositMoney:
         new_balance = next(acc["balance"] for acc in
                            customer_profile_after_change.json()["accounts"] if acc["id"] == 2)
 
-
         # Проверка кода ответа
         assert response.status_code == 200
 
@@ -51,8 +56,7 @@ class TestDepositMoney:
         # Проверка что баланс пользователя увеличился
         assert new_balance == old_balance + 100
 
-
-    #2. Депозит денег на второй аккаунт пользователя
+    # 2. Депозит денег на второй аккаунт пользователя
     def test_deposit_second_acc_user(self):
         # Получаем профиль до депозита
         customer_profile = requests.get(
@@ -80,8 +84,7 @@ class TestDepositMoney:
         )
 
         new_balance_acc2 = next(acc["balance"] for acc in
-                           customer_profile_after_change.json()["accounts"] if acc["id"] == 7)
-
+                                customer_profile_after_change.json()["accounts"] if acc["id"] == 7)
 
         # Проверка кода ответа
         assert response.status_code == 200
@@ -92,8 +95,19 @@ class TestDepositMoney:
         # Проверка, что баланс пользователя увеличился
         assert new_balance_acc2 == old_balance_acc2 + 150
 
-    #3. Депозит денег на несуществующий аккаунт
+    # 3. Депозит денег на несуществующий аккаунт
     def test_deposit_non_exist_acc(self):
+        # Проверка существующих счетов в банке
+
+        list_valid_accounts = requests.get(
+            url=f"{BASE_URL}/admin/users",
+            headers=AUTH_HEADER_ADMIN
+        )
+
+        valid_account_ids = [acc["id"] for user in list_valid_accounts.json() for acc in user["accounts"]]
+
+        assert 999 not in valid_account_ids
+
         # Делаем депозит на несуществующий аккаунт
         response = requests.post(
             url=f"{BASE_URL}/accounts/deposit",
@@ -110,14 +124,25 @@ class TestDepositMoney:
         # Проверка ошибки в ответе
         assert response.text.strip() == "Unauthorized access to account"
 
-    #4 Депозит с неверным id
-    @pytest.mark.parametrize("acc_id, expected_status",[
+    # 4 Депозит с неверным id
+    @pytest.mark.parametrize("acc_id, expected_status", [
         ("1", 500),
         (1.5, 200),
         (None, 500)
     ])
     # TODO Параметризация подогнана для того чтобы тесты были зелеными.
     def test_deposit_invalid_id(self, acc_id, expected_status):
+        # Проверка существующих счетов в банке
+
+        list_valid_accounts = requests.get(
+            url=f"{BASE_URL}/admin/users",
+            headers=AUTH_HEADER_ADMIN
+        )
+
+        valid_account_ids = [acc["id"] for user in list_valid_accounts.json() for acc in user["accounts"]]
+
+        assert 999 not in valid_account_ids
+
         # Делаем депозит c неверным id
         response = requests.post(
             url=f"{BASE_URL}/accounts/deposit",
@@ -131,7 +156,7 @@ class TestDepositMoney:
         # Проверка кода ответа
         assert response.status_code == expected_status
 
-#5 Депозит с неверным balance
+    # 5 Депозит с неверным balance
     @pytest.mark.parametrize("deposit_sum, expected_status, expected_error", [
         (0, 400, "Invalid account or amount"),
         (-1, 400, "Invalid account or amount")
@@ -152,11 +177,3 @@ class TestDepositMoney:
 
         # Проверка кода ответа
         assert response.text.strip() == expected_error
-
-
-
-
-
-
-
-
